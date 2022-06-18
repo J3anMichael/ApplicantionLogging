201f3b9ba5496c450687fa82fbb10ccd0691b814
Jean Michael <jeanmmclaudino@gmail.com>
2022-06-17 23:18:47 -0300

    Add project files.

---
+++ ApplicantionLogging/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using Microsoft.Extensions.Options;
+
+namespace ApplicationLoggingConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using ILoggerFactory loggerFactory =
+                LoggerFactory.Create(builder =>
+                    builder
+                    .AddFilter("ApplicationLoggingConsole.Program", LogLevel.Critical)
+                    .AddConsole(options =>
+                    {
+                        options.IncludeScopes = true;
+                        options.TimestampFormat = "dd - MM - yyyy hh: mm: ss";
+                    }));
+
+            ILogger logger = loggerFactory.CreateLogger<Program>();
+
+            logger.LogTrace("Log information message.");
+            logger.LogDebug("Log information message.");
+            logger.LogInformation("Log information message.");
+            logger.LogWarning("Log warning message.");
+            logger.LogError("Log error message.");
+            logger.LogCritical("Deu ruim.");
+        }
+    }
+
+    public class OptionsMonitor<T> : IOptionsMonitor<T>
+    {
+        private readonly T options;
+
+        public OptionsMonitor(T options)
+        {
+            this.options = options;
+        }
+
+        public T CurrentValue => options;
+
+        public T Get(string name) => options;
+
+        public IDisposable OnChange(Action<T, string> listener) => new NullDisposable();
+
+        private class NullDisposable : IDisposable
+        {
+            public void Dispose() { }
+        }
+    }
+}
